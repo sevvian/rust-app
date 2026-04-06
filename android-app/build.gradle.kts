f591e88fd8a1d48fdc61e3b7c78ab4a6c78a2cb1
sevvian <59226487+sevvian@users.noreply.github.com>
2026-04-05 20:01:38 -0700

    Update build.gradle.kts

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -1,7 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.mozilla.rust-android-gradle.rust-android") version "0.9.3" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
 }
 
 tasks.register<Delete>("clean") {
