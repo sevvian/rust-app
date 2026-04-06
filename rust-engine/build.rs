28ecb65f98c37664d6007963c604513e0af3c5c4
sevvian <59226487+sevvian@users.noreply.github.com>
2026-04-05 20:22:28 -0700

    Update build.rs

--- rust-engine/build.rs
+++ rust-engine/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // In 0.24.3, we use uniffi_build to generate the scaffolding
-    uniffi_build::generate_scaffolding("src/agent.udl")
+    // Generates the Rust scaffolding (the glue between C and Rust)
+    uniffi::generate_scaffolding("src/agent.udl")
         .expect("Failed to generate UniFFI scaffolding");
 }
